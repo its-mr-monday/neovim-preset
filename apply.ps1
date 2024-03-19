49219d873933b4feed99bdf70715d05f25c0fcdb
its-mr-monday <zackary.live8@gmail.com>
2024-03-19 17:08:17 -0400

    Windows apply script done:

--- apply.ps1
+++ apply.ps1
@@ -1,1 +1,21 @@
 
+
+#Directory is ~/AppData/Local/nvim
+
+# Get the current directory
+
+$path = $PSScriptRoot
+$currentDir = Get-Location
+Set-Location $path
+$nvimConfigPath = "$env:USERPROFILE\AppData\Local\nvim"
+
+#Step 1 remove anything in the old directory
+echo "Applying neovim configuration"
+mkdir $nvimConfigPath -Force
+Remove-Item $nvimConfigPath\* -Recurse -Force
+Set-Location $nvimConfigPath
+cp -r lua $nvimConfigPath
+cp init.lua $nvimConfigPath
+echo "Done"
+Set-Location $currentDir
+exit 0
